79f4cd5c3a643c3d4634def6fcf0329d57c01eb2
nichampion <47280962+nichampion@users.noreply.github.com>
2019-02-28 11:50:05 +0100

    Update fonctions_joueur.h

--- src/fonctions_joueur.h
+++ src/fonctions_joueur.h
@@ -1,8 +1,9 @@
 #ifndef fonctions_joueur_h
 #define fonctions_joueur_h
 
-#endif
-
+#define <string.h>
 t_joueur* cree_joueur(char nom[T]);
 void detruire_joueur(t_joueur **j);
 void calcul_totaux(t_joueur *j);
+
+#endif
