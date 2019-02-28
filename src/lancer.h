c7a38ca202b3f7bed7e20cf9fc1adf154958d792
Sunny <Sunny.Biard.Etu@univ-lemans.fr>
2019-02-28 11:21:43 +0100

    update lancer

---
+++ src/lancer.h
@@ -0,0 +1,5 @@
+#ifndef lancer_h
+#define lancer_h
+
+void lancer(t_joueur * j, int i);
+int relancer(t_joueur * j);
--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "affichage.h"
 #include "fonctions_joueur.h"
 #include "ordinateur.h"
+#include "lancer.h"
 
 
 /**
@@ -14,34 +15,6 @@
 	*\brief
 	*\version 1.0
 */
-void lancer(t_joueur * j, int i){
-
-	srand(time(NULL));
-	j->des[i] = rand()%(7-1) + 1;
-
-}
-
-int relancer(t_joueur * j){
-
-	int i, relance, rep;		
-
-	printf("Voulez vous relancer ? (0 : Non, 1 : Oui) ");
-	scanf("%d",&rep);
-
-	if(rep == 1){
-		for(i = 0; i < 5; i++){
-			printf("Voulez vous relancer le dé n°%d ? (0 : Non, 1 : Oui) ", i+1);
-			scanf("%d", &relance);
-				
-			if(relance == 1){
-				lancer(j,i);
-			}
-		}
-		return 1;
-	}
-
-	return 0;
-}
 
 int main(int argc, char **argv) {
 	t_joueur *j1, *j2;
