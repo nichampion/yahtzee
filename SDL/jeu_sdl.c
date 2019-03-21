b688aee6b8d57e57979bd5886220d878e9ecdf60
Sunny <Sunny.Biard.Etu@univ-lemans.fr>
2019-03-21 12:04:16 +0100

    SDL

--- SDL/jeu_sdl.c
+++ SDL/jeu_sdl.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "SDL2/include/SDL2/SDL.h"
-#include "SDL2/include/SDL2/SDL_ttf.h"
-#include "SDL2/include/SDL2/SDL_image.h"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_ttf.h"
+#include "SDL2/SDL_image.h"
 #include "commun.h"
 #include "fonctions_jeu.h"
 #include "fonctions_joueur.h"
