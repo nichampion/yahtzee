509e5b6ce8a5205ead8ba8e4ef53a89346ea9ffe
Champion Nicolas <s171568@ic2s122-08.univ-lemans.fr>
2019-04-02 09:24:51 +0200

    Modification structure pour la SDL

--- SDL/commun.h
+++ SDL/commun.h
@@ -1,53 +1,53 @@
 #ifndef commun_h
 #define commun_h
 
-#define N 5 /* Nombre de des */
-#define T 3 /* Taille nom joueur */
+	#define N 5 /* Nombre de des */
+	#define T 3 /* Taille nom joueur */
 
-/**
-  *\file commun.h
-  *\brief
-	*\version 1.0
-*/
+	/* ***** Constantes pour le tableau ***** */
 
-/**
-	*\struct t_tableau
-	*\brief Feuille de marque du Yahtzee
-	*\brief Correspond a un joueur, pour une partie.
-*/
+	#define TAILLE_TAB 18
 
 	/* Section supérieure */
-	/**< tab[0] : Total des 1 */
-	/**< tab[1] : Total des 2 */
-	/**< tab[2] : Total des 3 */
-	/**< tab[3] : Total des 4 */
-	/**< tab[4] : Total des 5 */
-	/**< tab[5] : Total des 6 */
-	/**< tab[6] : Bonus section supérieure (+35pts si total de la borne supérieure >= 63) */
+	#define AS 0 /**< Total des 1 */
+	#define DEUX 1 /**< Total des 2 */
+	#define TROIS 2 /**< Total des 3 */
+	#define QUATRE 3 /**< Total des 4 */
+	#define CINQ 4 /**< Total des 5 */
+	#define SIX 5	/**< Total des 6 */
+	#define PRIME_35 6 /**< +35pts si total_Sup >= 63 */
 
 	/* Section inférieure */
-	/**< tab[7] : (Brelan) Total des 5 des : 3 chiffres X + 2 autres quelconques */
-	/**< tab[8] : (Carre) Total des 5 des : 4 chiffres X + 1 chiffres Y */
-	/**< tab[9] : (Full) 25 points : 3 chiffres X + 2 chiffres Y */
-	/**< tab[10] : (Petite Suite) 30 points (4 des) */
-	/**< tab[11] : (Grande Suite) 40 points (5 des) */
-	/**< tab[12] : (Yahtzee) 50 points :  5 chiffres identiques */
-	/**< tab[13] : (Chance) Total des 5 des */
-	/**< tab[14] : (Prime Yahtzee) 100 points */
+	#define BRELAN 7 /**< Total des 5 des : 3 chiffres X + 2 autres quelconques */
+	#define CARRE 8 /**< Total des 5 des : 4 chiffres X + 1 chiffres Y */
+	#define FULL 9 /**< 25 points : 3 chiffres X + 2 chiffres Y */
+	#define PETITE_SUITE 10 /**< 30 points (4 des) */
+	#define GRANDE_SUITE 11 /**< 40 points (5 des) */
+	#define YAHTZEE 12 /**< 50 points :  5 chiffres identiques */
+	#define CHANCE 13 /**< Total des 5 des */
+	#define PRIME_YAHTZEE 14 /**< 100 points */
 
 	/* Totaux */
-	/**< tab[15] : Total de la borne supérieure */
-	/**< tab[16] : Total de la borne inférieure */
-	/**< tab[17] : Total global */
+	#define TOTAL_SUP 15
+	#define TOTAL_INF 16
+	#define TOTAl_GEN 17
 
-/**
-	*\struct t_joueur
-	*\brief Type joueur avec sa feuille de marque et des
-*/
-typedef struct {
-	int tab[18];
-	int des[N];
-	char nom[T];
-} t_joueur;
+	/* ************************************** */
+
+	/**
+	  *\file commun.h
+	  *\brief
+		*\version 1.0
+	*/
+
+	/**
+		*\struct t_joueur
+		*\brief Type joueur avec sa feuille de marque et des
+	*/
+	typedef struct {
+		int tab[TAILLE_TAB];
+		int des[N];
+		char nom[T];
+	} t_joueur;
 
 #endif
