cc0a836eb93c73e2978fff623f0e35f707ef0b6a
Champion Nicolas <s171568@ic2s122-08.univ-lemans.fr>
2019-04-02 09:52:50 +0200

    Ajout menu

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@
 	*\Author : Sunny BIARD
 */
 
+
 /**
 	*\Fn : void tour_joueur(t_joueur * j)
 	*\Param : Un pointeur sur un joueur
@@ -50,15 +51,8 @@ void tour_joueur(t_joueur * j){
 		placement = choix_placement(j,j_choix);
 }
 
-int main(int argc, char **argv) {
-	srand(time(NULL));
 
-/*
-	t_joueur * j1, * j2;
-
-	j1 = creer_joueur("J1");
-	j2 = creer_joueur("J2");
-*/
+void joueur_ordi() {
 	t_joueur * jh, * jo;
 
 	jh = creer_joueur("H");
@@ -66,22 +60,6 @@ int main(int argc, char **argv) {
 
 	int game_over = 0;
 
-/*
-	while(game_over == 0){
-
-		tour_joueur(j1);
-		affichage(j1,j2,j1);
-		sleep(5);
-		tour_joueur(j2);
-		affichage(j1,j2,j1);
-		sleep(5);
-
-		if(fin_de_partie(j1) && fin_de_partie(j2))
-			game_over = 1;
-
-	}
-	affichage(j1,j2,j1);
-*/
 	while(game_over == 0){
 
 		tour_joueur(jh);
@@ -96,6 +74,53 @@ int main(int argc, char **argv) {
 
 	}
 	affichage(jh,jo,jh);
+
+}
+
+
+void deux_joueurs() {
+	t_joueur * j1, * j2;
+
+	j1 = creer_joueur("J1");
+	j2 = creer_joueur("J2");
+
+	int game_over = 0;
+
+	while(game_over == 0){
+
+		tour_joueur(j1);
+		affichage(j1,j2,j1);
+		sleep(5);
+		tour_joueur(j2);
+		affichage(j1,j2,j1);
+		sleep(5);
+
+		if(fin_de_partie(j1) && fin_de_partie(j2))
+			game_over = 1;
+
+	}
+	affichage(j1,j2,j1);
+}
+
+
+int main(int argc, char **argv) {
+	srand(time(NULL));
+
+	int choix;
+
+	do {
+		printf("***** Yahtzee *****\n");
+		printf("\t1 - Humain VS Humain\n");
+		printf("\t2 - Humain VS Ordinateur\n");
+		printf("Choix : ");
+		scanf("%d", &choix);
+	} while(choix != 1 && choix != 2);
+
+	switch (choix) {
+		case 1 : deux_joueurs(); break;
+		case 2 : joueur_ordi(); break;
+		default : printf("\nErreur (contactez l'assistance)\n");
+	}
 
 	return 0;
 }
