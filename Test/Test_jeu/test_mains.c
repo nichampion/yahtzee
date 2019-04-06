ebb5c2f6f8b02c1d8171496cc5908a8ca5ee7398
ThibaultLMUnivMaine <Thibault.Lemarchand.etu@univ-lemans.fr>
2019-04-06 16:30:36 +0200

    Tests Fonctions jeu (70%)

---
+++ Test/Test_jeu/test_mains.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "../../src/commun.h"
+#include "../../src/fonctions_jeu.h"
+#include "../../src/fonctions_joueur.h"
+#include "../../src/mains.h"
+
+int main(){
+  t_joueur *j1;
+  j1 = creer_joueur("Joueur");
+  t_joueur *jtest;
+  jtest = creer_joueur("Test");
+  int i;
+
+  for(i=0;i<N;i++){
+    lancer(j1,i);
+  }
+
+  test_mains(j1,jtest);
+
+  printf("\nSi aucune possibilités, alors fonction test_mains incorrecte\n");
+}
